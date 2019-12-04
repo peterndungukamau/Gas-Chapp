4e0f1e6c13a2df30468a79adaa4c5de648f1ebd9
Nyakinyua <wanyakinyua968@gmail.com>
2019-12-04 14:37:37 +0300

    initialize database and create an instance

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
-from app import create_app
-from flask_script import Manager, Server
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import Admin
+
 
 app = create_app('development')
 
@@ -8,7 +10,7 @@ manager.add_command('server', Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app)
+    return dict(app = app,Admin=Admin)
     
 if __name__ == '__main__':
     manager.run()
