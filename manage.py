ae44049390fbab15364ce7b138a1a5d9bb070806
Nyakinyua <wanyakinyua968@gmail.com>
2019-12-04 17:35:55 +0300

    install flask wtf forms

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import Admin
+from app.models import Vendor
+from  flask_migrate import Migrate, MigrateCommand
+
 
 
 app = create_app('development')
@@ -8,9 +10,12 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server', Server)
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
 @manager.shell
 def make_shell_context():
-    return dict(app = app,Admin=Admin)
+    return dict(app = app,db=db,Vendor=Vendor)
     
 if __name__ == '__main__':
     manager.run()
