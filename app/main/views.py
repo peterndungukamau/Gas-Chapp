899276d7b9a605e34d290557d9124bdf8a6d26ef
peterndungukamau <peterndungukamau9@gmail.com>
2019-12-05 14:23:13 +0300

    create a function for add to cart

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,38 @@
 from . import main
 from flask import render_template
 
+
+orders = [
+    {
+        'img' : 'img',
+        'vendor' : 'k-gas',
+        'location' : '3200',
+        'Price' : '3200',
+
+
+    },
+
+        {
+        'img' : 'img',
+        'vendor' : 'k-gas',
+        'location' : '3200',
+        'Price' : '3200',
+
+    }
+]
+
 @main.route('/')
 def index():
-    return render_template('index.html')
+    title = 'Gas-Chapp'
+    return render_template('index.html', title=title,)
+
+@main.route('/order')
+def order():
+    title = 'Gas-Chapp'
+    return render_template('order.html', title=title, orders=orders)
+
+
+
+
+    
+
