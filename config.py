53eddcafe9fe1fbeadf816d6f08306896df4726d
Nyakinyua <wanyakinyua968@gmail.com>
2019-12-05 17:44:37 +0300

    add configurations for the uploads

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nyakinyua:Lastman@localhost/gas'
-
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
