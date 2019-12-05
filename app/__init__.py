2ff623a1bab38070cffbba9de1156ab14e0af524
Nyakinyua <wanyakinyua968@gmail.com>
2019-12-05 17:44:58 +0300

    initialize and create instance of db

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,17 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+login_manager.login_view = 'vendor.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
-
+photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
@@ -23,9 +25,16 @@ def create_app(config_name):
     login_manager.init_app(app)
     
     
+    
+    # configure UploadSet
+    configure_uploads(app,photos)
+    
     # register auth blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
+    
+    from .vendor import vendor as vendor_blueprint
+    app.register_blueprint(vendor_blueprint,url_prefix='/vendor')
     
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
