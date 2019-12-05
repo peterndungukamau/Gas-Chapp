337f8423b20527ab22d088e71e67718edf3e9d47
njoroge33 <johngichuhi769@gmail.com>
2019-12-05 14:31:05 +0300

    create migration

--- manage.py
+++ manage.py
@@ -1,11 +1,15 @@
 from app import create_app, db
 from flask_script import Manager, Server
+from  flask_migrate import Migrate, MigrateCommand
 from app.models import Cart
 
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server', Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
