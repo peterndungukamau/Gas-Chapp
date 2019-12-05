032b9c7fd9ff33eab3176e9e5b376c4a9ce20b48
Nyakinyua <wanyakinyua968@gmail.com>
2019-12-05 11:15:25 +0300

    import abort function to stop a request and return a rsponse as per the status code

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,19 @@
 from . import main
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
+from flask_login import login_required
+from ..models import Vendor
+
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = Vendor.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
